f3622336de498c8dee2387f267e44957b173ede6
frogger35 <nooo1230@gmail.com>
2019-09-27 15:40:42 -0500

    inital player Make it so that collision lower HP by some amount

---
+++ Player_Health.gd
@@ -0,0 +1,19 @@
+extends ProgressBar
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var player = get_node("../KinematicBody2D")
+	if(!player.dead):
+		self.rect_position.x = player.position.x-(self.rect_size.x/2)
+		self.rect_position.y = player.position.y-86/2-(self.rect_size.y+10)
+	else:
+		get_parent().remove_child(self)
+	#pass
