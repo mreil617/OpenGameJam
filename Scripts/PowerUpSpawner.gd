a772871165dd3ef4aafdf421bee5b1b80275ede6
Marcus Reilly <mreil94@gmail.com>
2019-09-29 13:31:15 -0400

    removed boilerplate

--- Scripts/PowerUpSpawner.gd
+++ Scripts/PowerUpSpawner.gd
@@ -1,27 +1,19 @@
 extends Node
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var initalSpawn = 3
 var SpawnRate = 10
 var lastSpawn = 1
-var HealthPackPre
+var HealthPackPre = preload("res://Prefabs/HealthPack.tscn")
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	HealthPackPre = preload("res://Prefabs/HealthPack.tscn")
 	for i in range(initalSpawn):
 		spawn_hp()
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+		
 func _process(delta):
 	lastSpawn+=delta
 	if int(lastSpawn) % SpawnRate  == 0:
 		lastSpawn+=1
 		spawn_hp()
-#	pass
 
 func spawn_hp():
 	print("spawn")
