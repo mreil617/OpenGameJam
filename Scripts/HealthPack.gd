d04041c577de4399e86c4c9bbac93bb480397535
frogger35 <nooo1230@gmail.com>
2019-09-29 19:08:21 -0500

    gameOver screen kinda

--- Scripts/HealthPack.gd
+++ Scripts/HealthPack.gd
@@ -1,11 +1,19 @@
 extends Area2D
 
+var time = 0
+
+func _process(delta):
+	if(!self.visible):
+		time += delta
+		if(time/delta >= 4):
+			self.visible = true
+
 func _on_HealthPack_body_entered(body):
 	if(body.get_parent().name == "NavNode"):
 		get_parent().powerupsOut -= 1
 		get_parent().remove_child(self)
 	else:
-		if(self.visible):
+		if(body.get_parent().name == "Player"):
 			body.get_parent().takeDamage(-50)
 			print("body entered")
 			self.visible = false
